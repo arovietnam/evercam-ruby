fbe2fe652b6319fcc9b9d55c3b517e17a2da8861
Tomasz Jama <gvs666@gmail.com>
2014-07-11 11:28:21 +0100

    added thumbnails - public fix

--- lib/evercam/public.rb
+++ lib/evercam/public.rb
@@ -9,11 +9,9 @@ module Evercam
     # critera::  A Hash of search criteria to use for the list returned
     #            by the request. Currently recognised options include
     #            :case_sensitive, :id_starts_with, :id_ends_with,
-    #            :id_contains, :offset and :limit.
-    # thumbnail::  A boolean to indicate if thumbnails should be included in
-    #          the fetch. Defaults to false.
-    def get_public_cameras(criteria={}, thumbnail=false)
-      data = handle_response(call("/public/cameras", :get, criteria.merge({thumbnail: thumbnail})))
+    #            :id_contains, :offset, :thumbnail and :limit.
+    def get_public_cameras(criteria={})
+      data = handle_response(call("/public/cameras", :get, criteria))
       unless data.include?("cameras")
         message = "Invalid response received from server."
         @logger.error message
