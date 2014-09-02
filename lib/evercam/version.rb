68c2807dfb2f5e60a38a3823e73cdfdb25d6d610
mosic <mosic.milos@gmail.com>
2014-09-02 18:46:53 +0200

    Updated webhook calls

--- lib/evercam/version.rb
+++ lib/evercam/version.rb
@@ -1,5 +1,5 @@
 # Copyright © 2014, Evercam.
 
 module Evercam
-  VERSION = "0.2.3"
+  VERSION = "0.2.4"
 end
--- lib/evercam/webhooks.rb
+++ lib/evercam/webhooks.rb
@@ -6,29 +6,18 @@ module Evercam
     # This method fetches details for all webhooks that belong to specific camera.
     #
     # ==== Parameters
+    # id::         The unique identifier of the webhook.
     # camera_id::  The unique identifier of the camera to be queried for webhooks.
-    def get_webhooks(camera_id)
-      data = handle_response(call("/webhooks?id=#{camera_id}")) unless camera_id.nil?
+    def get_webhooks(camera_id, id = nil)
+      parameters = {camera_id: camera_id}
+      parameters[:id] = id if id
+      data = handle_response(call("/webhooks", :get, parameters)) unless camera_id.nil?
       if !data.include?("webhooks")
         message = "Invalid response received from server."
         @logger.error message
         raise EvercamError.new(message)
       end
       data["webhooks"]
-    end
-
-    # This method attempts to retrieve the details for a specific webhook.
-    #
-    # ==== Parameters
-    # webhook_id::  The unique identifier for the webhook to query.
-    def get_webhook(webhook_id)
-      data = handle_response(call("/webhooks/#{webhook_id}"))
-      if !data.include?("webhooks") || data["webhooks"].size == 0
-        message = "Invalid response received from server."
-        @logger.error message
-        raise EvercamError.new(message)
-      end
-      data["webhooks"][0]
     end
 
     # This method attempts to update the details associated with a camera.
@@ -64,8 +53,8 @@ module Evercam
     # id::          The unique identifier of the camera.
     # url::         The url which will receive webhook data.
     # user_id::     The Evercam user name of the webhook owner.
-    def create_webhook(id, url, user_id)
-      parameters = {id: id,
+    def create_webhook(camera_id, url, user_id)
+      parameters = {camera_id: camera_id,
                     url: url,
                     user_id: user_id}
 
